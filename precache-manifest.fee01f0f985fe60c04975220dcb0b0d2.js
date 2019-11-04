3e2e556bd93ce0e7a84eb26a48ac8dde2aede221
romal <mohey.romal@gmail.com>
2019-11-04 12:14:11 +0100

    Updates

---
+++ precache-manifest.fee01f0f985fe60c04975220dcb0b0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b80f24dffaa83bd77d651bbd4321f94",
+    "url": "/github.io/my-react-app/index.html"
+  },
+  {
+    "revision": "dcd6cc9a5bc60a97f6ba",
+    "url": "/github.io/my-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a801a616269755283f67",
+    "url": "/github.io/my-react-app/static/js/2.5302f875.chunk.js"
+  },
+  {
+    "revision": "dcd6cc9a5bc60a97f6ba",
+    "url": "/github.io/my-react-app/static/js/main.32486f9b.chunk.js"
+  },
+  {
+    "revision": "d00ffbd0f4fa859b34a9",
+    "url": "/github.io/my-react-app/static/js/runtime-main.fd9c4f80.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github.io/my-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
